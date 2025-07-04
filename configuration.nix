49a27c8a3cd32c94c6017b28949421a399b06402
Russ <russ@gigs.com>
2025-07-04 17:47:34 +0200

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,205 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      # <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+      # <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+      ./hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.initrd.luks.devices."luks-ac02f74f-6a17-4401-b85a-fee50635a6e9".device = "/dev/disk/by-uuid/ac02f74f-6a17-4401-b85a-fee50635a6e9";
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/boot/crypto_keyfile.bin" = null;
+  };
+
+  boot.loader.grub.enableCryptodisk = true;
+
+  boot.initrd.luks.devices."luks-f34090ce-5d56-4d5d-b625-704e0a345df8".keyFile = "/boot/crypto_keyfile.bin";
+  boot.initrd.luks.devices."luks-ac02f74f-6a17-4401-b85a-fee50635a6e9".keyFile = "/boot/crypto_keyfile.bin";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.russellc = {
+    isNormalUser = true;
+    description = "Russell";
+    extraGroups = [ "networkmanager" "wheel" "video" ];
+    packages = with pkgs; [];
+  };
+
+  home-manager.users.russellc = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      alacritty
+      librewolf
+      mako
+      rofi
+    ];
+    programs.bash.enable = true;
+    programs.rofi.enable = true;
+    programs.waybar.enable = true;
+    services.mako.enable = true;
+
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        font = {
+          size = 12.0;
+          normal.family = "Iosevka Term SS09";
+        };
+      };
+    };
+
+    programs.librewolf = {
+      enable = true;
+      settings = {
+        "webgl.disabled" = false;
+        "privacy.resistFingerprinting" = false;
+        "privacy.clearOnShutdown.history" = false;
+        "privacy.clearOnShutdown.cookies" = false;
+        "sidebar.position_start" = false;
+        "sidebar.revamp" = true;
+        "sidebar.verticalTabs" = true;
+        "ui.systemUsesDarkTheme" = true;
+      };
+    };
+
+    # programs.neovim = {
+    #   enable = true;
+    #   package = pkgs.neovim;
+    # };
+
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        terminal = "alacritty";
+        menu = "rofi -terminal alacritty -show combi -combi-modes drun#run -modes combi";
+        bars = [{ command = "waybar"; }];
+      };
+    };
+
+    home.stateVersion = "25.05";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+  #  wget
+  ];
+
+  environment.etc."greetd/environments".text = ''
+    niri-session
+    sway
+  '';
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  programs.niri.enable = true;
+  programs.sway.enable = true;
+
+  # List services that you want to enable:
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
+        user = "greeter";
+      };
+    };
+  };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.blueman.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  security.polkit.enable = true;
+  security.pam.services.swaylock = {};
+  security.rtkit.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  hardware.graphics.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
