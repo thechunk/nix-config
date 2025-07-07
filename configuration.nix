9bdc449372d70a6b345bfe5e502b75b41522d82b
Russ <russ@gigs.com>
2025-07-07 22:13:28 +0200

    add console font, hostname

--- configuration.nix
+++ configuration.nix
@@ -22,7 +22,7 @@ in
   # Use latest kernel.
   # boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "russnix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -34,6 +34,8 @@ in
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
+
+  console.font = "Lat2-Terminus16";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
